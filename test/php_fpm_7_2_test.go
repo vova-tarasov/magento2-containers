ef4256315e3399059576ca4cf080f9b59f7d609f
vova tarasov <volodymyr.tarasov@gmail.com>
2020-07-21 19:13:36 +0300

    Add Travis support

--- test/php_fpm_7_2_test.go
+++ test/php_fpm_7_2_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestDev72HasAllPHPModules(t *testing.T) {
+func TestDev72(t *testing.T) {
 	tag := "magento2-php-fpm-72-test"
 	args := []string{
 		"BUILD_ENVIRONMENT_IMAGE=magento2-php-fpm-development-onbuild",
@@ -20,33 +20,22 @@ func TestDev72HasAllPHPModules(t *testing.T) {
 
 	docker.Build(t, "..", buildOptions)
 
+  // PHP version
 	opts := &docker.RunOptions{Command: []string{"bash", "-c", "php -r 'echo PHP_VERSION;'"}}
 	output := docker.Run(t, tag, opts)
 	assert.Regexp(t, "^7\\.2\\.", output)
 
+  // Contains required PHP modules
 	opts = &docker.RunOptions{Command: []string{"php", "-m"}}
 	output = docker.Run(t, tag, opts)
 	assert.Equal(t, PHP_MODULES, output)
-}
 
-func TestDev72HasCronSupport(t *testing.T) {
-	tag := "magento2-php-fpm-72-test"
-	args := []string{
-		"BUILD_ENVIRONMENT_IMAGE=magento2-php-fpm-development-onbuild",
-	}
-
-	buildOptions := &docker.BuildOptions{
-		Tags:         []string{tag},
-		BuildArgs:    args,
-		OtherOptions: []string{"-f", "../build/php-fpm/Dockerfile"},
-	}
-
-	docker.Build(t, "..", buildOptions)
-
-	opts := &docker.RunOptions{Command: []string{"which", "supercronic"}}
-	output := docker.Run(t, tag, opts)
+  // Contains supercronic
+	opts = &docker.RunOptions{Command: []string{"which", "supercronic"}}
+	output = docker.Run(t, tag, opts)
 	assert.Equal(t, "/usr/local/bin/supercronic", output)
 
+  // Contains tini
 	opts = &docker.RunOptions{Command: []string{"which", "tini"}}
 	output = docker.Run(t, tag, opts)
 	assert.Equal(t, "/sbin/tini", output)
--- test/php_fpm_7_3_test.go
+++ test/php_fpm_7_3_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestDev73HasAllPHPModules(t *testing.T) {
+func TestDev73(t *testing.T) {
 	tag := "magento2-php-fpm-73-test"
 	args := []string{
 		"BUILD_ENVIRONMENT_IMAGE=magento2-php-fpm-development-onbuild",
@@ -21,34 +21,22 @@ func TestDev73HasAllPHPModules(t *testing.T) {
 
 	docker.Build(t, "..", buildOptions)
 
+  // PHP version
 	opts := &docker.RunOptions{Command: []string{"bash", "-c", "php -r 'echo PHP_VERSION;'"}}
 	output := docker.Run(t, tag, opts)
 	assert.Regexp(t, "^7\\.3\\.", output)
 
+  // Contains required PHP modules
 	opts = &docker.RunOptions{Command: []string{"php", "-m"}}
 	output = docker.Run(t, tag, opts)
 	assert.Equal(t, PHP_MODULES, output)
-}
 
-func TestDev73HasCronSupport(t *testing.T) {
-	tag := "magento2-php-fpm-73-test"
-	args := []string{
-		"BUILD_ENVIRONMENT_IMAGE=magento2-php-fpm-development-onbuild",
-		"PHP_FPM_IMAGE=php:7.3-fpm-alpine",
-	}
-
-	buildOptions := &docker.BuildOptions{
-		Tags: []string{tag},
-		BuildArgs: args,
-		OtherOptions: []string{"-f", "../build/php-fpm/Dockerfile"},
-	}
-
-	docker.Build(t, "..", buildOptions)
-
-	opts := &docker.RunOptions{Command: []string{"which", "supercronic"}}
-	output := docker.Run(t, tag, opts)
+  // Contains supercronic
+	opts = &docker.RunOptions{Command: []string{"which", "supercronic"}}
+	output = docker.Run(t, tag, opts)
 	assert.Equal(t, "/usr/local/bin/supercronic", output)
 
+  // Contains tini
 	opts = &docker.RunOptions{Command: []string{"which", "tini"}}
 	output = docker.Run(t, tag, opts)
 	assert.Equal(t, "/sbin/tini", output)
